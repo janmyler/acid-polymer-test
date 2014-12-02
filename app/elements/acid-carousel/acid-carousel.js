0538f88e44010bbd34b2b4802b400ab26b45af84
Jan Myler <honza.myler@gmail.com>
2014-12-02 15:01:32 +0100

    Added basic template for carousel

--- app/elements/acid-carousel/acid-carousel.js
+++ app/elements/acid-carousel/acid-carousel.js
@@ -5,7 +5,15 @@
 (function (Polymer) {
   'use strict';
 
+  var ANIMATIONS = ['none', 'fade', 'slide'];
+
   Polymer({
+    autoplay: true,
+    duration: 5000,
+    animation: 'fade',
+    height: 500,
+    controls: 'true',
+    items: []
   });
 
 })(Polymer);
