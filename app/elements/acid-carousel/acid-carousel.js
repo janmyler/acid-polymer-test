ffe9e758aebf110dc365229f4162ab8be972c69e
Jan Myler <honza.myler@gmail.com>
2014-12-02 16:15:22 +0100

    Carousel init, prev/next actions

--- app/elements/acid-carousel/acid-carousel.js
+++ app/elements/acid-carousel/acid-carousel.js
@@ -5,15 +5,45 @@
 (function (Polymer) {
   'use strict';
 
-  var ANIMATIONS = ['none', 'fade', 'slide'];
-
   Polymer({
     autoplay: true,
     duration: 5000,
     animation: 'fade',
-    height: 500,
+    height: '500px',
     controls: 'true',
-    items: []
+    animations: ['none', 'fade', 'slide'],
+    items: [],
+    selected: 0,
+    ready: function() {
+      // set carousel's height
+      this.style.height = this.height;
+
+      // get titles
+      this.$.images.items.forEach(function(elem, ind) {
+        this.items.push({
+          title: elem.getAttribute('title'),
+          index: ind
+        })
+      }.bind(this));
+
+      // check the animation type and force the default one if needed
+      if (this.animations.indexOf(this.animation) < 0) {
+        this.animation = 'fade';
+      }
+    },
+    nextAction: function() {
+      this.selected = (this.selected + 1) % this.items.length;
+    },
+    prevAction: function() {
+      if (this.selected === 0) {
+        this.selected = this.items.length;
+      }
+
+      this.selected -= 1;
+    },
+    seekAction: function(e, detail, target) {
+      console.log(e, detail, target);
+    }
   });
 
 })(Polymer);
